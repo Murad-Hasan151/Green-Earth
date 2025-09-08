dc6d80734b6a1020a053a9d8d11bf90626e8e568
Murad-Hasan <mdmuradhasan151@gmail.com>
2025-09-08 18:43:32 +0600

    completed load all trees functionalies

--- script.js
+++ script.js
@@ -27,11 +27,27 @@ const loadAllCard = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            showAllCard(data.plants);
         })
         .catch(err => {
             console.log(err)
         })
+}
+const showAllCard = (cards) => {
+    cards.forEach(card => {
+        allCard.innerHTML += `
+                    <div class="p-3 bg-white rounded-lg"> 
+                        <img class="h-60 w-full rounded-lg" src="${card.image}" alt="">
+                        <h4 class="font-semibold text-lg mt-2">${card.name}</h4>
+                        <p class=" my-1">${card.description}</p>
+                        <div class="flex justify-between items-center mt-2">
+                            <button class="bg-[#DCFCE7] text-[#15803D] font-semibold px-3 py-1 rounded-3xl">${card.category}</button>
+                            <p class"font-bold"><span>${card.price}</span> TK</p>
+                        </div>
+                        <button class=" text-lg font-semibold mt-5 bg-[#15803D] text-white px-5 py-2 rounded-3xl w-full">Add to Cart</button>
+                    </div>
+        `;
+    })
 }
 
 
